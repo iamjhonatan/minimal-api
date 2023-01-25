350202908ff9bd7176af13a15c994a593c64c2a6
Jhonatan Medeiros <jhonatanmedeiros7@gmail.com>
2023-01-25 20:24:33 -0300

    class 51: creating a minimal API- I

--- MinimalAPI/Program.cs
+++ MinimalAPI/Program.cs
@@ -1,16 +1,25 @@
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(x =>
+{
+    x.SwaggerDoc("v1", new OpenApiInfo { Title = "Minimal API", Version = "v1" });
+});
 
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(x => x.SwaggerEndpoint("/swagger/v1/swagger.json", "Minimal API v1"));
 }
 
 app.MapGet("/", () => "Hello world!");
+
+app.MapGet("sentences", async () =>
+    await new HttpClient().GetStringAsync("https://ron-swanson-quotes.herokuapp.com/v2/quotes")
+);
 
 app.Run();
